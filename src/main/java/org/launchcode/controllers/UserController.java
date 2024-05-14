47be2790682d151f7b094789c5a8cdd592c0a50b
Matt Stephens <mdstephens33@hotmail.com>
2024-05-13 20:41:40 -0500

    Spa day signup 2 Studio Done without Bonus

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,34 +1,38 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/add")
-    public String displayAddUserForm() {
-        return "user/add";
+    @GetMapping
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        return "/user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        }
-        else {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+//        model.addAttribute("user", user);
+//        model.addAttribute("verify", verify);
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add User");
+            return "user/add";
+       } else if (!user.getPassword().equals(verify)) {
+            model.addAttribute("title", "Add User");
             model.addAttribute("error", "Passwords do not match");
             return "user/add";
+        } else {
+            return "user/index";
+            }
         }
 
     }
-
-
-}
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,31 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+    @Email(message = "Invalid email. Try again.")
     private String email;
+    @NotBlank
+    @Size(min =6, message="Password must be at least 6 characters.")
     private String password;
+    @NotBlank
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,6 +50,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
 
